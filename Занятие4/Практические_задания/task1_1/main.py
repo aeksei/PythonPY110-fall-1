947ee5bec8229fd0f0162f457ce1e05d091cb3b0
alekseyp <aeksei94@gmail.com>
2021-12-01 21:27:00 +0300

    1.12.21

--- Занятие4/Практические_задания/task1_1/main.py
+++ Занятие4/Практические_задания/task1_1/main.py
@@ -11,11 +11,12 @@ def task():
         ", s,dfsdf",
         "123, fewfew",
     ]
-
-    word_pattern = re.compile(...)  # TODO записать регулярное выражение для поиска слова любой длины
+    regex = r"\w+"
+    word_pattern = re.compile(regex)  # TODO записать регулярное выражение для поиска слова любой длины
 
     for word in word_list:
-        print(word_pattern)  # TODO вызвать от регулярного выражения методы search и group
+        # print(re.search(regex, word).group())
+        print(word_pattern.search(word).group())  # TODO вызвать от регулярного выражения методы search и group
 
 
 if __name__ == "__main__":
